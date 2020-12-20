9676601900ea506ddc6efb21c82e36b370509e9b
apio1130 <apio1130@naver.com>
2020-12-20 12:36:54 +0900

    fix(detail): fix css for movie detail
    - fix css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
     componentDidMount() {
@@ -11,7 +12,26 @@ class Detail extends React.Component {
     render() {
         const { location } = this.props;
         if (location.state) {
-            return <span>{location.state.title}</span>;
+            return (
+                <div className="movie_detail__container">
+                    {/* <span>{location.state.title}</span> */}
+                    <div className="movie_detail__img">
+                        <img src={location.state.poster} alt={location.state.title} title={location.state.title}></img>
+                    </div>
+                    <div className="movie_detail__data">
+                        <h3 className="movie_detail__title">{location.state.title}</h3>
+                        <h5 className="movie_detail__year">{location.state.year}</h5>
+                        <ul className="movie_detail__genres">
+                            {location.state.genres.map((genre, index) => (
+                                <li key={index} className="movie__genre">
+                                    {genre}
+                                </li>
+                            ))}
+                        </ul>
+                        <p className="movie_detail__summary">{location.state.summary}</p>
+                    </div>
+                </div>
+            );
         } else {
             return null;
         }
